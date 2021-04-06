86961d2692f060744f574f986d8bc1d6437ee637
EduardoValPla <aquilestra@gmail.com>
2021-04-06 17:29:37 -0500

    editar y borrar estructura

--- php/section/areas.php
+++ php/section/areas.php
@@ -3,13 +3,15 @@ $areas = $db->prepare('SELECT * FROM areasdetrabajo WHERE borrado = 0');
 $areas->execute([]);
 
 echo '<table id="myTable">';
-echo '<thead><tr><th> ID </th><th> Nombre </th></tr></thead>';
+echo '<thead><tr><th> ID </th><th> Nombre </th><th></th><th></th></tr></thead>';
 echo '<tbody>';
 
 foreach($areas as $area) {
     echo '<tr>';
     echo '<td>' . htmlentities($area['id']) . '</td>';
     echo '<td>' . htmlentities($area['nombre']) . '</td>';
+    echo '<td> Editar</td>';
+    echo '<td> Borrar</td>';
     echo '</tr>';
 }
 
