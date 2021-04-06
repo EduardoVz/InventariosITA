91ccab19b5f77ba92bfdd8fc4a279a25ec8edd2c
EduardoValPla <aquilestra@gmail.com>
2021-04-06 17:19:11 -0500

    correccion de areas

--- php/section/areas.php
+++ php/section/areas.php
@@ -1,10 +1,4 @@
 <?php
-if ($_GET['accion'] == 'login') echo '<section id="login">';
-else echo '<section>';
-
-if ($_GET['accion'] == 'login') echo 'login';
-else if ($_GET['accion'] == 'perfil') {
-echo 'perfil';
 $post = $db->prepare('SELECT * FROM areasdetrabajo');
 $post->execute([]);
 echo '<table id="myTable"> ';
@@ -29,9 +23,4 @@ foreach($post as $posta){
     echo '</tbody> 
       </table>';
     
-
-}
-else if ($_GET['accion'] == 'error') header("Location: /");
-
-echo '</section>';
 ?>
