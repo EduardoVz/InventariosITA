3c1e4ad8285c4de6cba2fae8f6cd11aca34913a4
EduardoValPla <aquilestra@gmail.com>
2021-04-11 13:46:43 -0500

    editar y borrar registros de tabla inventario

--- php/section/procesar.php
+++ php/section/procesar.php
@@ -24,5 +24,17 @@ if ($_POST['procesar'] == 'login') {
     $result = $db->prepare('INSERT INTO `historial`(`descripcion`) VALUES ("Se modifico '.$_POST['nombre'].', de la tabla '.$_POST['tabla'].' por '.$_SESSION['usuario'].'")');
     $result->execute([]);
     header('Location: /'.$_POST['tabla'].'/');
+} else if ($_POST['procesar'] == 'borrarinventario') {
+    $result = $db->prepare('UPDATE '.$_POST['tabla'].' SET borrado=\'1\' WHERE id=\''.$_POST['id'].'\'');
+    $result->execute([]);
+    $result = $db->prepare('INSERT INTO `historial`(`descripcion`) VALUES ("Se elimino '.$_POST['descripcion'].', de la tabla. '.$_POST['tabla'].' por '.$_SESSION['usuario'].'")');
+    $result->execute([]);
+    header('Location: /'.$_POST['tabla'].'/');
+} else if ($_POST['procesar'] == 'actualizarinventario') {
+    $result = $db->prepare('UPDATE '.$_POST['tabla'].' SET `No SEP`="'.$_POST['nosep'].'", `No Inventario`="'.$_POST['noinventario'].'", `id_area`="'.$_POST['id_area'].'" , `id_resguardante`="'.$_POST['id_resguardante'].'", `Descripcion`="'.$_POST['descripcion'].'", `Marca`="'.$_POST['marca'].'", `Modelo`="'.$_POST['modelo'].'", `Serie`="'.$_POST['serie'].'", `Valor`="'.$_POST['valor'].'", `Fecha`="'.$_POST['fecha'].'" WHERE id=\''.$_POST['id'].'\'');
+    $result->execute([]);
+    $result = $db->prepare('INSERT INTO `historial`(`descripcion`) VALUES ("Se modifico '.$_POST['noinventario'].', de la tabla '.$_POST['tabla'].' por '.$_SESSION['usuario'].'")');
+    $result->execute([]);
+    header('Location: /'.$_POST['tabla'].'/');
 }
 ?>
